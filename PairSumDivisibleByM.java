d2529d30ca6aa3a80fb2e10653b9afc74ba541f4
Praveen Balasundaram <88590426+prvnbala@users.noreply.github.com>
2022-08-14 18:13:18 +0530

    Create PairSumDivisibleByM.java

---
+++ PairSumDivisibleByM.java
@@ -0,0 +1,45 @@
+// Pair Sum divisible by M
+
+// Given an array of integers A and an integer B, find and return the number of pairs in A whose sum is divisible by B.
+
+// Since the answer may be large, return the answer modulo (109 + 7).
+
+// 1 <= length of the array <= 100000
+// 1 <= A[i] <= 10^9
+// 1 <= B <= 10^6
+
+class Solution {
+
+    //solution idea: (a+b) % c = (a%c + b%c) % c;
+    int mod = 1000 * 1000 * 1000 + 7;
+
+    public int solve(ArrayList<Integer> A, int B) {
+        Map<Integer, Integer> modValueCounter = new HashMap<>();
+        for(int i = 0; i < A.size(); i++) {
+            int x = A.get(i);
+            int modValue = x % B;
+            int currentCount = modValueCounter.containsKey(modValue) ? modValueCounter.get(modValue) : 0;
+            modValueCounter.put(modValue, currentCount + 1);
+            A.set(i, modValue);
+        }
+
+        long ans = 0;
+        for(int x : A) {
+            int xCompliment = (B - x) % B;
+            if(modValueCounter.containsKey(x) && modValueCounter.containsKey(xCompliment)) {
+                int xCount = modValueCounter.get(x);
+                int xComplimentCount = modValueCounter.get(xCompliment);
+                long totalPairs = 0;
+                if(x == xCompliment) {
+                    totalPairs = ((1L * xCount * (xCount - 1)) / 2) % mod;
+                } else {
+                    totalPairs = (1L * xCount * xComplimentCount) % mod;
+                    modValueCounter.remove(xCompliment);
+                }
+                ans = (ans + totalPairs) % mod;
+                modValueCounter.remove(x);
+            }
+        }
+        return (int) ans;
+    }
+}
