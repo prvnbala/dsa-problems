24872726a710c89f0ff3bb2f9036c5b9cea26908
Praveen Balasundaram <88590426+prvnbala@users.noreply.github.com>
2022-08-14 21:13:04 +0530

    Create SmallerAndGreater.java

---
+++ SmallerAndGreater.java
@@ -0,0 +1,19 @@
+// You are given an Array A of size N.
+
+// Find for how many elements, there is a strictly smaller element and a strictly greater element.
+
+class Solution {
+    public int solve(ArrayList<Integer> A) {
+        
+        int counter = 0; 
+        int max = Collections.max(A);
+        int min = Collections.min(A);
+        
+        for(Integer x: A) {
+            if(x>min && x<max) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
