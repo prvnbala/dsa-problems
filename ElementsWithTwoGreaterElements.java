f0eb26de0c608cf5721e3103ee60b3fc40917405
Praveen Balasundaram <88590426+prvnbala@users.noreply.github.com>
2022-08-14 19:21:46 +0530

    Create ElementsWithTwoGreaterElements.java

---
+++ ElementsWithTwoGreaterElements.java
@@ -0,0 +1,31 @@
+// You are given an array of distinct integers A, you have to find and return all elements in array which have at-least two greater elements than themselves.
+
+// NOTE: The result should have the order in which they are present in the original array.
+
+// 3 <= |A| <= 10^5
+
+// -10^9 <= A[i] <= 10^9
+
+class Solution {
+    public ArrayList<Integer> solve(ArrayList<Integer> A) {
+        int firstMax = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        for(int x : A) {
+            if(x > firstMax) {
+                secondMax = firstMax;
+                firstMax = x;
+            } else if (x > secondMax) {
+                secondMax = x;
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        for(int x : A) {
+            if(x < secondMax) {
+                result.add(x);
+            }
+        }
+
+        return result;
+    }
+}
