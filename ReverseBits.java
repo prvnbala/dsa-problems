b08f8b009fe183acb57a2126e903a0a2ea67a56b
Praveen Balasundaram <88590426+prvnbala@users.noreply.github.com>
2022-08-14 21:47:57 +0530

    Create ReverseBits.java

---
+++ ReverseBits.java
@@ -0,0 +1,19 @@
+// Reverse the bits of an 32 bit unsigned integer A.
+// 0 <= A <= 2^32
+
+class Solution {
+	public long reverse(long a) {
+        long result = 0;
+        for(int i = 0; i < 32; i++) {
+            result = result << 1;
+            if(isCurrentBitSet(a, i)) {
+                result = result | 1; //set the last bit in result
+            }
+        }
+        return result;
+	}
+
+    private boolean isCurrentBitSet(long a, int i) {
+        return (a & (1 << i)) > 0;
+    }
+}
