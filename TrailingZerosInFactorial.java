843d59c6e5cab67a8532b52c6b83f30836ec2cfb
Praveen Balasundaram <88590426+prvnbala@users.noreply.github.com>
2022-08-17 08:02:34 +0530

    Create TrailingZerosInFactorial.java

---
+++ TrailingZerosInFactorial.java
@@ -0,0 +1,15 @@
+// Given an integer A, return the number of trailing zeroes in A! i.e., factorial of A.
+// Note: Your solution should run in logarithmic time complexity.
+
+// 1 <= A <= 10^9
+
+class Solution {
+    public int trailingZeroes(int A) {
+        int result = 0;
+        while(A > 0) {
+            result += A / 5;
+            A /= 5;
+        }
+        return result;
+    }
+}
