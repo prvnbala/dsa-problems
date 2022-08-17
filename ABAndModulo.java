67811e33b64d535834b7d6f20e6314bf5ef745f8
Praveen Balasundaram <88590426+prvnbala@users.noreply.github.com>
2022-08-17 07:58:59 +0530

    Create ABAndModulo.java

---
+++ ABAndModulo.java
@@ -0,0 +1,9 @@
+// Given two integers A and B, find the greatest possible positive integer M, such that A % M = B % M.
+// 1 <= A, B <= 10^9
+// A != B
+
+class Solution {
+    public int solve(int A, int B) {
+        return Math.abs(A-B);
+    }
+}
