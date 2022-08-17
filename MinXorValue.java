9d9a72fdc9151779f830e48d72dbeff6f46e2e5d
Praveen Balasundaram <88590426+prvnbala@users.noreply.github.com>
2022-08-17 21:39:09 +0530

    Create MinXorValue.java

---
+++ MinXorValue.java
@@ -0,0 +1,19 @@
+// Given an integer array A of N integers, 
+// find the pair of integers in the array which have minimum XOR value. 
+// Report the minimum XOR value.
+
+// 2 <= length of the array <= 100000
+// 0 <= A[i] <= 10^9
+
+class Solution {
+    public int findMinXor(ArrayList<Integer> A) {
+        int minXor = Integer.MAX_VALUE;
+        Collections.sort(A);
+        for(int i = 1; i < A.size(); i++) {
+            int currentElement = A.get(i);
+            int previousElement = A.get(i - 1);
+            minXor = Math.min(minXor, previousElement^currentElement);
+        }
+        return minXor;
+    }
+}
