ae768865f746ae5a21dceb0fb906883fffb641a0
Praveen Balasundaram <88590426+prvnbala@users.noreply.github.com>
2022-08-18 19:54:30 +0530

    Create TimeToEquality.java

---
+++ TimeToEquality.java
@@ -0,0 +1,15 @@
+// Given an integer array A of size N. In one second, you can increase the value of one element by 1.
+// Find the minimum time in seconds to make all elements of the array equal.
+// 1 <= N <= 1000000
+// 1 <= A[i] <= 1000
+
+class TimeToEquality {
+    public int solve(ArrayList<Integer> A) {
+        int maxValue = Collections.max(A);
+        int result = 0;
+        for(int x : A) {
+            result += (maxValue - x);
+        }
+        return result;
+    }
+}
