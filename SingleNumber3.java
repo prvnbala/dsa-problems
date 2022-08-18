0e6158c89c220046362d93822ec5c0b3f7537018
Praveen Balasundaram <88590426+prvnbala@users.noreply.github.com>
2022-08-18 19:52:54 +0530

    Create SingleNumber3.java

---
+++ SingleNumber3.java
@@ -0,0 +1,55 @@
+// Given an array of positive integers A, two integers appear only once, and all the other integers appear twice.
+// Find the two integers that appear only once.
+
+// Note: Return the two numbers in ascending order.
+
+// 2 <= |A| <= 100000
+// 1 <= A[i] <= 10^9
+
+
+class SingleNumber3 {
+    public ArrayList<Integer> solve(ArrayList<Integer> A) {
+        
+        int xorValue = getXorOfAllElementsInArray(A); //cancels duplicates and have xor of two single numbers
+        int position = findLeftMostSetBitPosition(xorValue); //find first bit difference in the two single numbers
+
+        int singleNumberWithSetBitAtPosition = 0;
+        int singleNumberWithUnsetBitAtPosition = 0;
+
+        for(int x : A) {
+            if(isBitSetAtPosition(x, position)) {
+                singleNumberWithSetBitAtPosition ^= x;
+            } else {
+                singleNumberWithUnsetBitAtPosition ^= x;
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>(Arrays.asList(
+            singleNumberWithSetBitAtPosition, 
+            singleNumberWithUnsetBitAtPosition));
+        
+        Collections.sort(result);
+        return result;
+    }
+
+    private int getXorOfAllElementsInArray(ArrayList<Integer> A) {
+        int xorValue = 0;
+        for(int x : A) {
+            xorValue ^= x;
+        }
+        return xorValue;
+    }
+
+    private int findLeftMostSetBitPosition(int value) {
+        for(int i = 0; i < 32; i++) {
+            if(isBitSetAtPosition(value, i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean isBitSetAtPosition(int value, int position) {
+        return (value & (1 << position)) > 0;
+    }
+}
