cbc7a74e508e86be26e8d84489942ec220639940
praveen <praveen.balasundram@gmail.com>
2022-08-27 22:07:16 +0530

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,39 @@
+// Given an array of integers, find two numbers such that they add up to a specific target number.
+
+// The function twoSum should return indices of the two numbers such that they add up to the target,
+// where index1 < index2. Please note that your returned answers (both index1 and index2 )
+// are not zero-based. Put both these numbers in order in an array and return the array
+// from your function ( Looking at the function signature will make things clearer ).
+// Note that, if no pair exists, return empty list.
+
+// If multiple solutions exist, output the one where index2 is minimum.
+// If there are multiple solutions with the minimum index2,
+// choose the one with minimum index1 out of them.
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TwoSum {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public ArrayList<Integer> twoSum(final List<Integer> A, int B) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> visitedElements = new HashMap<>();
+
+        for(int i = 0; i < A.size(); i++) {
+            int oneBasedIndex = i + 1;
+            int number = A.get(i);
+            int match = B - number;
+            if(visitedElements.containsKey(match)) {
+                result.add(visitedElements.get(match));
+                result.add(oneBasedIndex);
+                return result;
+            } else {
+                //put if absent is important, since key may already present.
+                visitedElements.putIfAbsent(number, oneBasedIndex);
+            }
+        }
+        return result;
+    }
+}
